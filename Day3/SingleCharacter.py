a267584832ae400c189d3ad5f33f7cab4088d959
OGOLA-SOSPETER <captain483sos@gmail.com>
2023-05-15 13:11:37 +0300

    #Day3 Single Character Check.

---
+++ Day3/SingleCharacter.py
@@ -0,0 +1,19 @@
+#Program to check if the input is a single character.
+
+def IsSingle(mword):
+    if len(mword) == 1:
+        return True
+    else:
+        return False
+
+
+
+if __name__ == "__main__":
+    while True:
+        get_word = input('Enter the letter.')
+        if IsSingle(get_word):
+            print("The valid character is ", get_word)
+
+        else:
+            print('Invalid Characters!!')
+
